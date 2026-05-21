791604780504f7cdda831529f287693bf018cf56
BeniAlexis23 <beni_luyo2303@outlook.com>
2026-05-21 09:09:58 -0500

    update cronograma

--- frontend/src/components/services.jsx
+++ frontend/src/components/services.jsx
@@ -5,7 +5,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Cronograma de Actividades</h2>
           <p>
-            Cronograma de actividades del evento, donde podrás encontrar la programación de las diferentes actividades que se llevarán a cabo el día 05/06/2025, en la Sede Académica de la Casa de la Cultura.
+            Cronograma de actividades del evento, donde podrás encontrar la programación de las diferentes actividades que se llevarán a cabo el día 10/06/2026, en la Sede Académica de la Casa de la Cultura.
           </p>
         </div>
         <div className="row">
